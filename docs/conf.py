fbfa5c64225faab22c09b8fde1a3615731226e3f
Ruslan Kid <kidryachevrr@gmail.com>
2022-11-11 12:41:43 +0500

    modified struct'

---
+++ docs/conf.py
@@ -0,0 +1,78 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# Full list of options can be found in the Sphinx documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+from typing import Any, Dict
+
+# add the demo python code to the path, so that it can be used to demonstrate
+# source links
+sys.path.append(os.path.abspath("./kitchen-sink/demo_py"))
+
+# -- Project information -----------------------------------------------------
+#
+
+project = "CV"
+#copyright = "2020, Pradyun Gedam"
+author = "Ruslan Kid"
+
+# -- General configuration ---------------------------------------------------
+#
+
+extensions = [
+    # Sphinx's own extensions
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    # Our custom extension, only meant for Furo's own documentation.
+    "furo.sphinxext",
+    # External stuff
+    #"sphinx_design",
+    #"sphinx_inline_tabs",
+]
+templates_path = ["_templates"]
+
+# -- Options for extlinks ----------------------------------------------------
+#
+
+extlinks = {
+    "pypi": ("https://pypi.org/project/%s/", "%s"),
+}
+
+# -- Options for intersphinx -------------------------------------------------
+#
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
+}
+
+# -- Options for TODOs -------------------------------------------------------
+#
+
+todo_include_todos = True
+
+# -- Options for Markdown files ----------------------------------------------
+#
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+myst_heading_anchors = 3
+
+# -- Options for HTML output -------------------------------------------------
+#
+
+html_theme = "furo"
+html_title = "CV"
+html_logo = "logo.png"
+language = "ru"
+
+html_static_path = ["_static"]
+html_css_files = ["pied-piper-admonition.css"]
